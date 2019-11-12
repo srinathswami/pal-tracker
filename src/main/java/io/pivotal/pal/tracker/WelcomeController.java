807a097e79021affc17d20892d3cbda35290a32d
srinathswami <srinathswami@gmail.com>
2019-11-12 12:05:34 +0530

    travis

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    private String welcomeMessage;
+    public WelcomeController(@Value("${welcome.message}") String a_welcome_message) {
+        this.welcomeMessage = a_welcome_message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMessage;
     }
 }
--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -1,6 +1,7 @@
 package test.pivotal.pal.tracker;
 
 import io.pivotal.pal.tracker.WelcomeController;
+import org.assertj.core.api.AbstractCharSequenceAssert;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,6 +12,6 @@ public class WelcomeControllerTest {
     public void itSaysHello() throws Exception {
         WelcomeController controller = new WelcomeController("A welcome message");
 
-        assertThat(controller.sayHello()).isEqualTo("A welcome message");
+        AbstractCharSequenceAssert<?, String> a_welcome_message = assertThat(controller.sayHello()).isEqualTo("A welcome message");
     }
 }
